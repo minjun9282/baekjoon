5cec47162254705bed1a1802d84a5c1a82ec5a4e
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2021-02-06 14:13:10 +0900

    Update 4673.cpp

--- baekjoon/4673.cpp
+++ baekjoon/4673.cpp
@@ -8,7 +8,7 @@ bool self_number(int n);
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
 	int i = 1;
 	while(i < 10001) {
 		if (self_number(i) == true) {
@@ -19,6 +19,7 @@ int main() {
 			i++;
 		}
 	}
+
 
 	return 0;
 }
@@ -32,20 +33,26 @@ int sum_of_digit(int n) {
 		input = input % int(pow(10, index));
 		index--;
 	}
+	result += n;
 	return result;
 }
 
 bool self_number(int n) {
-	int index = floor(log10(n));
-	int minimum = n - index * 9;
-
-	int i = n;
-	while (i > minimum - 1) {
-		if (i + sum_of_digit(i) == n) {
+	int index = floor(log10(n)) + 1; // 두자리수면 2, 한자리수면 1이 나옴.
+	int minimum = n - index * 9; // 여기서 minimum을 어떻게 설정하느냐에 따라 연산속도에서 차이발생함.
+	int i;
+	if (index == 1) {
+		i = 1;
+	}
+	else {
+		i = minimum;
+	}
+	while (i < n) {
+		if (sum_of_digit(i) == n) {
 			return false;
 		}
 		else {
-			i--;
+			i++;
 		}
 	}
 	return true;
