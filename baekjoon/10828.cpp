3ee391d8e19889c4d493389eec1fb9b6be0d9c0c
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2021-05-24 15:27:24 +0900

    Add files via upload

---
+++ baekjoon/10828.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// class나 함수로 stack을 구현하지 않은 버전
+
+int main(){
+    int N;
+    cin >> N;
+    int* stack = new int[N];
+    int i = 0;
+    int point = -1;
+    string order;
+    int number;
+
+
+    while(i < N){
+        cin >> order;
+        if(order == "push"){
+            cin >> number;
+            point++;
+            stack[point] = number;
+        }
+        else if(order == "pop"){
+            if(point > -1){
+                cout << stack[point] << '\n';
+                point--;
+            }
+            else{
+                cout << -1 << '\n';
+            }
+        }
+        else if(order == "size"){
+            cout << point + 1 << '\n';
+        }
+        else if(order == "empty"){
+            if(point > -1){
+                cout << 0 << '\n';
+            }
+            else{
+                cout << 1 << '\n';
+            }
+        }
+        else if(order == "top"){
+            if(point > -1){
+                cout << stack[point] << '\n';
+            }
+            else{
+                cout << -1 << '\n';
+            }
+        }
+    i++;
+    }
+    
+    delete[] stack;
+
+    return 0;
+}
