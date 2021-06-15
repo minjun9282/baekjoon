f9bfa6978f78b77c6fb4e9c620e9141dfd9ad25f
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2021-06-15 19:48:32 +0900

    Add files via upload

---
+++ baekjoon/14501_incomplete.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using  namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	cin >> N;
+	int* T_i = new int[N];
+	int* P_i = new int[N];
+	int i = 0;
+	while (i < N) {
+		cin >> T_i[i] >> P_i[i];
+	}
+	int days, revenue = 0;
+
+	int k, j = 0;
+	int temp = 0;
+	while (j < N) {
+		days = j;
+		temp += 
+		while (k < N) {
+
+		}
+		j++;
+	}
+
+	return 0;
+}
