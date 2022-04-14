672b84ffd3a1d7e97c0689bf2bc28d132f87264a
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2022-04-14 09:25:24 +0900

    Add files via upload

---
+++ baekjoon/10926.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string id;
+    cin >> id;
+    cout << id << "?\?!" << '\n';
+
+    return 0;
+}
---
+++ baekjoon/18108.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int y;
+    cin >> y;
+    cout << y - 543 << '\n';
+
+    return 0;
+}
