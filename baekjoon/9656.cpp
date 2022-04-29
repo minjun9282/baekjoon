04e846c32ffcc450d7ccbcdc3eb35bad85f3c222
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2022-04-29 09:35:58 +0900

    Update 9656.cpp

--- baekjoon/9656.cpp
+++ baekjoon/9656.cpp
@@ -6,6 +6,8 @@ int main(){
     cin.tie(NULL);
     int N;
     cin >> N;
+    //창영이는 4k나 4k-2를 만들 수 있는 입장
+    //상근이는 4k-1이나 4k-3을 만들 수 있는 입장
     if(N % 4 == 0 || N % 4 == 2){
         cout << "SK" << '\n';
     }
