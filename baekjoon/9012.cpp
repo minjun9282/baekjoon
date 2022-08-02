f26a179f720169c3974d0a68a38fd578974d627d
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2022-08-02 09:48:46 +0900

    Add files via upload

--- baekjoon/9012.cpp
+++ baekjoon/9012.cpp
@@ -1,28 +1,67 @@
 #include <iostream>
-#include <string>
+#include <stack>
 using namespace std;
 
-bool VPS(string input);
-
 int main(){
-    int T;
-    string input;
-    for(int i = 0; i < T; i++){
-        cin >> input;
-        VPS(input);
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int T;
+    bool VPS = true;
+    cin >> T;
+    cin.ignore();
+
+    stack<char> vps;
+    char ps[50];
+    int i = 0;
+    int j = 0;
+
+    while(i < T){
+        cin.getline(ps, 51); //여기서 문자열을 50개까지 받으려면 51을 써줘야함!
+        while(true){
+            if(ps[j] == '\0'){
+                break;
+            }
+            else{
+                if(ps[j] == '('){
+                    vps.push('(');
+                    j++;
+                }
+                else{
+                    if(vps.size() == 0){
+                        VPS = false;
+                        break;
+                    }
+                    else{
+                        if(vps.top() == '('){
+                            vps.pop();
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+        if(vps.size() == 0 && VPS == true){
+            cout << "YES" << '\n';
+        }
+        else{
+            cout << "NO" << '\n';
+        }
+        // j = 0, vps 스택, bool 변수 VPS 초기화 해줘야함.
+
+        /*
+        for(int k = 0; k < vps.size(); k++){   //이방식으로 할 경우 예제 입력 1의 2번째 경우가 초기화가 제대로 안됨.
+            vps.pop();
+        }
+        */
+
+        while(!vps.empty()){
+            vps.pop();
+        }
+        VPS = true;
+        j = 0;
+        i++;
+    }
 
     return 0;
-}
-
-bool VPS(string input){
-    bool result;
-
-    if(result == true){
-        cout << "YES" << '\n';
-    }
-    else{
-        cout << "NO" << '\n';
-    }
 }
