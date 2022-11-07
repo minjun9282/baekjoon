b8bae291bb8d01c2d47040e2b3c532f01013d6a1
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2022-11-07 13:11:33 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	cin >> N;
+	int temp, i = 1;
+	cin >> temp;
+	int min = temp;
+	int max = temp;
+	while (i < N) {
+		cin >> temp;
+		if (temp > max) {
+			max = temp;
+		}
+		if (temp < min) {
+			min = temp;
+		}
+		i++;
+	}
+
+	cout << min * max << '\n';
+	
+
+	return 0;
+}
