c3a0393ff398976af7e99b14c378766bafbd1566
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2024-07-04 17:48:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int arr[n + 1] = {0};
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 2;
+    for (int i = 3; i < n + 1; i++){
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+    }
+    cout << arr[n] << '\n';
+    
+    return 0;
+}
