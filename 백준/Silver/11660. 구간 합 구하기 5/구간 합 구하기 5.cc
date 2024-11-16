2c541b07e508960efa036796f76e76ed805aabb0
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2024-11-16 22:09:53 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 120 ms, Memory: 10356 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int N, M;
     cin >> N >> M;
-    vector<vector<int>> table(N, vector<int>(N, 0));
-    int x;
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            cin >> x;
-            if (j > 0){
-                table[i][j] = table[i][j-1] + x;
-            }
-            else{
-                table[i][j] += x;
-            }
+
+    vector<vector<int>> arr(N + 1, vector<int>(N + 1, 0));
+    vector<vector<int>> dp(N + 1, vector<int>(N + 1, 0));
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> arr[i][j];
         }
     }
-    while(M--){
-        int x1, y1, x2, y2; 
-        cin >> x1 >> y1 >> x2 >> y2;
-        int result = 0;
-        for (int i = x1 - 1; i <= x2 - 1; i++){
-            result += (table[i][y2-1] - table[i][y1-2]);
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            dp[i][j] = arr[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
         }
-        cout << result << '\n';
+    }
+
+    for (int q = 0; q < M; q++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        int result = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+
+        cout << result << "\n";
     }
 
     return 0;
